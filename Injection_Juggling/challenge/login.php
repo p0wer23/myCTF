d53265ac80cf7b7f8b8284d4236ec33fe766274f
Punith K <punithk03@gmail.com>
2023-09-21 17:20:44 +0530

    Modify login.php

--- Injection_Juggling/challenge/login.php
+++ Injection_Juggling/challenge/login.php
@@ -9,19 +9,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT id FROM users WHERE Role = ? and passhash = ?";
+    $sql = "SELECT passhash FROM users WHERE Role = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $username, $passhash);
+    $stmt->bind_param("s", $username);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows===1) {
-        session_start();
-        $_SESSION['Role'] = $username;
-        header("Location: home.php");
+        $row = $result->fetch_assoc();
+        if ($row['passhash']==$passhash){
+            session_start();
+            $_SESSION['Role'] = $username;
+            header("Location: home.php");
+            exit;
+        }
+        echo "Wrong password";
         exit;
     } else {
-        echo "Invalid username or password. Please try again.";
+        echo "Invalid username. Please try again.";
     }
 }
 ?>
